76a38ea091055ccaee665471b67d01f648eec81d
Louis <2130377@cegepmontpetit.ca>
2023-05-12 21:14:20 -0400

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,6 +61,8 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+
+      defaultSidebarValue: 'docs/index',
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
@@ -130,7 +132,7 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
+        theme: darkCodeTheme,
         darkTheme: darkCodeTheme,
       },
     }),
